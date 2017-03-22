82aa6dadf07eafc62a77a67172738c5a593b360e
joegabonito <joejung5678@gmail.com>
2017-03-22 03:17:16 -0400

    Updated Version

---
+++ src/main/resources/static/js/admin/posts/postForm.js
@@ -0,0 +1,17 @@
+$("document").ready(function(){
+	
+	/*
+	 * This JQuery function only allows one category of Accounting for a post.
+	 */
+	$('select').change(function() {
+		if($( this ).hasClass("postformlist") ){
+		$(this)
+	        .parent('div').parent('div').siblings('div').find('.postformlist')
+	        .attr('disabled', true).end()
+		}
+	    if(this.value == "default")
+	        $(this)
+	        .parent('div').parent('div').siblings('div').find('.postformlist')
+	        .attr('disabled', false)
+	});
+}); 
